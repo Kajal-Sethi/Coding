1a11769a8deb0df4c5f68f2719cc6ee0e0bbc5f4
Kajal-Sethi <63057127+Kajal-Sethi@users.noreply.github.com>
2022-06-30 15:48:49 +0530

    Time: 24 ms (17.14%), Space: 11 MB (5.28%) - LeetHub

---
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        // shift to the median/mean of the array
+        int i, median, count=0, n=nums.size();
+        sort(nums.begin(), nums.end());
+        
+        if(n%2==0)
+            median = ceil(float(nums[n/2] + nums[n/2-1])/2);
+        
+        else
+            median = nums[n/2];
+        
+//         int median = 
+//         for(i=0; i<nums.size(); i++)
+//             sum+=nums[i];
+        
+//         sum = floor(float(sum)/nums.size());
+         cout<<median;
+        
+        for(i=0; i<nums.size(); i++)
+            count+=abs(median-nums[i]);
+        
+        return count;
+    }
+};
