70100eea2cba847e310215584843db409705373c
Kajal-Sethi <63057127+Kajal-Sethi@users.noreply.github.com>
2022-07-04 16:00:17 +0530

    Time: 46 ms (22.25%), Space: 18.2 MB (23.59%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,45 +1,19 @@
 class Solution {
 public:
     int candy(vector<int>& rate) {
-        // use min heap
-        vector<int>candies(rate.size(),1);
+        vector<int> left(rate.size(),1), right(rate.size(),1);
         int i, need=0;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-          
-        for(i=0; i<rate.size(); i++)
-            pq.push({rate[i], i});
         
-        while(!pq.empty())
-        {
-            // val, index
-            pair<int, int> curr = pq.top();
-            pq.pop();
-            
-            if(curr.second+1<rate.size() && rate[curr.second]>rate[curr.second+1] && curr.second-1>=0 && rate[curr.second]>rate[curr.second-1])
-            {
-                // find konsa rate bada he 
-                int maxx=max(candies[curr.second+1], candies[curr.second-1]);
-                candies[curr.second] = maxx+1;
-                need+=maxx+1;
-            }
-            
-            else if(curr.second+1<rate.size() && rate[curr.second]>rate[curr.second+1])
-            {
-                candies[curr.second] = candies[curr.second+1]+1;
-                need+=candies[curr.second+1]+1;
-            }
-            
-            else if(curr.second-1>=0 && rate[curr.second]>rate[curr.second-1])
-            {
-                candies[curr.second] = candies[curr.second-1]+1;
-                need+=candies[curr.second-1]+1;
-            }
-            else
-            {    
-                need+=candies[curr.second];
-                continue;
-            }
-        }
+        for(i=1; i<rate.size(); i++)
+            if(rate[i]>rate[i-1])
+                left[i] = left[i-1]+1;
+        
+        for(i=rate.size()-2; i>=0; i--)
+            if(rate[i]>rate[i+1])
+                right[i] = right[i+1]+1;
+        
+        for(i=0; i<rate.size(); i++)
+            need = need+max(left[i], right[i]);
         
         return need;
     }
