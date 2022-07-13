f29d59ef3bda9489d7505d8c6a21c351a2818ee5
Kajal-Sethi <63057127+Kajal-Sethi@users.noreply.github.com>
2022-07-13 23:25:19 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (76.26%) - LeetHub

---
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        // use a stack and a q
+        
+        vector<vector<int>> ans;
+        if(root==NULL)
+            return ans;
+            
+        TreeNode* curr;
+        queue<TreeNode*> q;
+        stack<TreeNode*> st1, st2;
+        
+        st1.push(root);
+        
+        int turn =1, size;
+        while(!st1.empty() || !st2.empty())
+        {
+            vector<int> vec;
+            if(turn == 1)
+            {
+                while(!st1.empty())
+                {
+                    curr = st1.top();
+                    st1.pop();
+
+                    if(curr->left!=NULL)
+                        st2.push(curr->left);
+
+                    if(curr->right!=NULL)
+                        st2.push(curr->right);
+                    
+                    vec.push_back(curr->val);
+                }
+                
+                turn = 2;
+            }
+                
+            else
+            {
+                while(!st2.empty())
+                {
+                    curr = st2.top();
+                    st2.pop();
+                    
+                    if(curr->right!=NULL)
+                        st1.push(curr->right);
+
+                    if(curr->left!=NULL)
+                        st1.push(curr->left);
+
+                    vec.push_back(curr->val);
+                }               
+                
+                turn = 1;
+            }
+            
+            ans.push_back(vec);
+        }
+        
+        return ans;
+    }
+};
