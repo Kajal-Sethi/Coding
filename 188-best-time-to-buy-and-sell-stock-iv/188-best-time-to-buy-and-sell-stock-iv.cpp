a0fbdf3c6544cb5beabb60f962fc73b231a19d85
Kajal-Sethi <63057127+Kajal-Sethi@users.noreply.github.com>
2022-07-19 02:08:08 +0530

    Time: 12 ms (70.50%), Space: 12.1 MB (45.73%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     
-    int t[1001][101][2];
-    int solve(int times, vector<int>& arr, int i, int buy)
+    // buy=-1
+    // sell=1
+    int t[101][1001][3];
+    int solve(int k, vector<int>& prices, int index, int turn)
     {
-        if(times==0 || i>=arr.size())
-            return t[i][times][buy] = 0;
+        if(k==0 || index==prices.size())
+            return 0; 
         
-        if(t[i][times][buy]!=-1)
-            return t[i][times][buy];
+        if(t[k][index][turn+1]!=-1)
+            return t[k][index][turn+1];
         
-        // can buy
-        if(buy==1)
-            return t[i][times][buy] = max(solve(times, arr, i+1, buy), solve(times, arr, i+1, !buy)-arr[i]);
+        if(turn == -1)
+            return t[k][index][turn+1] = max(solve(k, prices, index+1, turn), solve(k, prices, index+1, -turn)+turn*prices[index]);
+        // dec k at sell
         
-        else 
-            return t[i][times][buy] = max(solve(times, arr, i+1, buy), solve(times-1, arr, i+1, !buy)+arr[i]);
+        else
+            return t[k][index][turn+1] = max(solve(k, prices, index+1, turn), solve(k-1, prices, index+1, -turn)+turn*prices[index]);
     }
-    
+        
     int maxProfit(int k, vector<int>& prices) {
-        memset(t, -1, sizeof(t));
-        return solve(k, prices, 0, 1);
+        memset(t,-1,sizeof(t));
+        return solve(k, prices, 0, -1);
     }
 };
