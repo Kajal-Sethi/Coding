39e9a593974bd02c8e6f1aedca3dc3050cc22ee2
Kajal-Sethi <63057127+Kajal-Sethi@users.noreply.github.com>
2022-07-24 19:28:35 +0530

    Time: 731 ms (50.00%), Space: 99.5 MB (50.00%) - LeetHub

---
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    long long countExcellentPairs(vector<int>& nums, int k) {
+        set <int> s{nums.begin(), nums.end()};
+        // count set bits for each number
+        
+        set<int>::iterator itr2;
+        map<int, int> mp;
+        
+        for (itr2 = s.begin(); itr2 != s.end(); itr2++) 
+        {
+            int curr=*itr2, count=0;
+            while(curr>0)
+            {
+                if(curr&1)
+                    count++;
+                curr/=2;
+            }
+            mp[count]++;
+        }
+        
+        long long ans=0;
+        
+        map<int, int>::iterator itr, use2;
+        for (itr = mp.begin(); itr != mp.end(); ++itr) {
+            for (use2 = mp.begin(); use2 != mp.end(); ++use2) {                
+                if(itr->first + use2->first >=k)
+                 {
+//                      if(itr->first != use2->first)
+//                          ans+=itr->second * use2->second *2;
+                     
+//                      else
+                         ans+=itr->second * use2->second;
+                    
+                    // cout<<itr->first<<" "<<itr->second<<"     "<<use2->second<<" "<<use2->first<<endl;
+                 }
+            }
+        }
+        return ans;
+        
+    }
+};
