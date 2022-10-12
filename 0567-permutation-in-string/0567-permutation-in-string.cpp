b9c9ca54629101d41e282755c501fc054063bc6e
Kajal-Sethi <63057127+Kajal-Sethi@users.noreply.github.com>
2022-10-12 05:55:26 +0530

    Time: 23 ms (42.49%), Space: 7.1 MB (89.72%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -8,40 +8,25 @@ public:
     if(P.size()>S.size())
         return false;
         
-    // cout<<P;
     vector<int> freq(26, 0), sec(26, 0);
-    int i, count=0, finder=0;
+    int i, st=0;
     
     for(i=0; i<P.size(); i++)
-    {
-        if(freq[P[i]-'a']==0)
-            count++;
-        
         freq[P[i]-'a']++;
-    }
     
-    int st=0;
     for(i=0; i<S.size(); i++)
     {
-        if(sec[S[i]-'a']==0)
-            finder++;
-        
         sec[S[i]-'a']++;
         
-        if(finder>=count)
+        if(i-st+1>=P.size())
         {
             // remove from st and compare with freq
             while(i-st+1>P.size())
             {
-                sec[S[st]-'a']--;
-                if(sec[S[st]-'a']==0)
-                    finder--;
-                
+                sec[S[st]-'a']--;                
                 st++;
             }
             
-            // if(i-st+1==P.size())
-            //     cout<<S.substr(st, P.size())<<endl;
             if(sec==freq)
                 return true;
         }
